19ca339571d2e23e7f2a59ca72c59f481a01d831
dbenitezc <62899899+dbenitezc@users.noreply.github.com>
2025-03-05 07:07:21 -0500

    nuevo server

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://notasseguras.netlify.app", // Reemplázalo con la URL de tu frontend en Netlify
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+}));
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
